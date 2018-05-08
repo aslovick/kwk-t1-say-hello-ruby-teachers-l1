8b8b86db20332bf9403d0881a46fab8f63927669
Abigail Slovick <abby.slovick@gmail.com>
2018-05-08 23:11:01 +0000

    automatically backed up by learn

--- spec/say_hello_spec.rb
+++ spec/say_hello_spec.rb
@@ -2,12 +2,4 @@
 def "say_hello.rb"
 end
 
-puts  it 'accepts an argument of a name and prints out Hello with that Name' do
-    expect($stdout).to receive(:puts).with("Hello Kent Beck!")
-    say_hello("Kent Beck")
-  end
 
-puts it 'defaults to Ruby Programmer when no name is passed in' do
-    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
-    say_hello()
-  end
